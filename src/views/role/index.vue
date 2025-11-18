de2d507a66e6c72029156cdae5dc0482fc3bc4dd
Alice <2162823020@qq.com>
2025-11-18 20:17:11 +0800

    角色管理页-----初步完成表单项行内编辑

--- src/views/role/index.vue
+++ src/views/role/index.vue
@@ -5,7 +5,12 @@
         <el-button type="primary" size="mini" @click="showdialog = true">添加角色</el-button>
       </div>
       <el-table :data="list">   <!--el-table的data属性为表格数据源,这里用的是获取的角色数组-->
-        <el-table-column label="角色" align="center" prop="name" />
+        <el-table-column label="角色" align="center" prop="name">
+          <template #default="{row}">
+            <el-input v-if="row.isedit" size="mini" />
+            <span v-else>{{ row.name }}</span>
+          </template>
+        </el-table-column>
         <el-table-column label="启用" align="center" prop="state"> <!--column 的prop属性为设置填充列的字段,这里设置了state设置完后会对象数组中的每个对象都会用state这个字段填充该列-->
           <template #default="{row}">                 <!--el-table里面内置了循环插槽渲染数据源数组的长度为多少就会启动多少次作用域插槽渲染,每次会向父组件的template传递每一行的数据row通过解构获取-->
             <span>{{ row.state === 1 ? '已启用':row.state === 0 ? '未启用': '无' }}</span>
@@ -13,9 +18,9 @@
         </el-table-column>
         <el-table-column label="表述" align="center" prop="description" />
         <el-table-column label="操作" align="center"> <!--center为让表头内容居中-->
-          <template>
+          <template #default="{row}">
             <el-button type="text" size="mini">分配权限</el-button>
-            <el-button type="text" size="mini">编辑</el-button>
+            <el-button type="text" size="mini" @click="edit(row)">编辑</el-button>
             <el-button type="text" size="mini">删除</el-button>
           </template> <!--默认插槽渲染次数取决于数据源数组长度-->
         </el-table-column>
@@ -78,6 +83,9 @@ export default {
       const { rows, total } = await getRoleList(this.pageParams) // 进入页面获取第一页数据,每页5条
       this.pageParams.total = total // 将获取到的数据总数赋值给分页对象的total
       this.list = rows // 将获取的角色数组赋值
+      this.list.forEach(item => {
+        this.$set(item, 'isedit', false) // 这里设置响应式属性,因为vue中对象手动添加的属性不具备响应式,故需要通过该方法添加属性
+      }) // 给角色对象数组中的每一个对象都添加编辑标记
     },
     pageChange(page) {
       this.pageParams.page = page // 切换页码时给params的page重新赋值,然后重新拉取数据
@@ -96,7 +104,10 @@ export default {
     btnCancel() {
       this.$refs.role.resetFields()
       this.showdialog = false // 需要注意的是如果有表单项(el-item)没有加prop属性则该表单项不会被重置
-    } // 取消
+    }, // 取消
+    edit(row) {
+      row.isedit = true // 将当前编程行的编辑状态改为true
+    }
   }
 }
 </script>
