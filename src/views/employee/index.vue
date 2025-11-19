7939144f7fbcbe576796ca31791ea58efd3e4623
Alice <2162823020@qq.com>
2025-11-19 20:01:23 +0800

    员工管理页----完成选中左树节点时,自动记录选中的节点

--- src/views/employee/index.vue
+++ src/views/employee/index.vue
@@ -5,12 +5,17 @@
         <el-input style="margin-bottom:10px" type="text" prefix-icon="el-icon-search" size="small" placeholder="输入员工姓名全员搜索" />
         <!-- 树形组件 -->
         <el-tree
+          ref="Tree"
           :data="organization"
           :props="defaultprops"
           :highlight-current="true"
           default-expand-all
           :expand-on-click-node="false"
+          node-key="id"
+          @current-change="selectNode"
         /> <!--highlight-current选中项高亮 expand-on-click-node点击节点时节点不折叠-->
+        <!--node-key 指定树节点使用哪个字段作为唯一的身份标识 -->
+        <!--current-change事件会在切换节点时触发,这里会向selectNode这个方法传递两个形参(当前节点的数据，当前节点的 Node 对象)-->
       </div>
       <div class="right">
         <el-row class="opeate-tools" type="flex" justify="end">
@@ -36,7 +41,10 @@ export default {
       defaultprops: {
         label: 'name',
         children: 'children'
-      }
+      },
+      queryParams: {
+        departmentId: ''
+      } // 获取员工查询参数对象
     }
   },
   created() {
@@ -45,7 +53,14 @@ export default {
   methods: {
     async getOrganizationData() {
       this.organization = transListToTreeData(await getDepartmentList(), 0) // 将线性组织数据转为树型数据
-    }
+      this.queryParams.departmentId = this.organization[0].id // 获取第一个节点id并记录下来
+      this.$nextTick(() => {
+        this.$refs.Tree.setCurrentKey(this.queryParams.departmentId) // 通过setCurrentKey节点默认选中第一个节点
+      }) // 因为数据渲染是异步的,所以需要等节点树渲染完毕,后再去选中节点
+    },
+    selectNode(node) {
+      this.queryParams.departmentId = node.id
+    } // node为当前选中节点数据,当选择节点时自动记录节点id
   }
 }
 </script>
