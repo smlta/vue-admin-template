92d78175e1f8651db34ec866303c654c7f355472
Alice <2162823020@qq.com>
2025-11-26 17:53:31 +0800

    员工权限页------完成布局的基本搭建

--- src/views/permission/index.vue
+++ src/views/permission/index.vue
@@ -1,7 +1,17 @@
 <template>
   <div class="container">
     <div class="app-container">
-      权限管理
+      <el-button type="primary" size="mini" class="btn-add">添加权限</el-button>
+      <el-table>
+        <el-table-column label="名称" />
+        <el-table-column label="标识" />
+        <el-table-column label="描述" />
+        <el-table-column label="操作">
+          <el-button type="text" size="mini">添加</el-button>
+          <el-button type="text" size="mini">编辑</el-button>
+          <el-button type="text" size="mini">删除</el-button>
+        </el-table-column>
+      </el-table>
     </div>
   </div>
 </template>
@@ -10,3 +20,9 @@ export default {
   name: 'Permission'
 }
 </script>
+<style scoped>
+ .btn-add {
+  margin: 10px;
+ }
+</style>
+
